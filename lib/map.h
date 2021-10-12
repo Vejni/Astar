5778c2a6ca173b5ce84c521ac0c427bc969398d3
Vejni <vmarcell000@gmail.com>
2021-10-12 13:33:44 +0200

    Implemented tokenizer

--- lib/map.h
+++ lib/map.h
@@ -1,5 +1,18 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+
+const char delim[] = "|";
+
+void process_line(char * line){
+  // Tokenize with delimiter |
+  char *tok = line;
+  char *end = line;
+  while ((tok = strsep(&end, delim)) != NULL){
+    printf("%s\n", tok);
+  }
+  printf("---------------\n");
+}
 
 void create_map(char * path){
   FILE * fp;
@@ -13,8 +26,10 @@ void create_map(char * path){
     exit(1);
   }
 
+  // Read line by line
   while ((read = getline(&line, &len, fp)) != -1) {
-    printf("%s", line);
+    if (line[0] == '#') continue;  // skip lines starting with #
+    process_line(line);
   }
 
   fclose(fp);
