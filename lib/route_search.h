a0fd0e986fca68996f6a022959ba975d61d5e005
jorgemedial <jorgemedial@gmail.com>
2021-10-31 12:11:52 +0100

    updated .gitignore and lib/route_search.h

--- lib/route_search.h
+++ lib/route_search.h
@@ -1,6 +1,26 @@
-
 #include 'map.h'
 
-typedef struct Pq{
-
+// Priority Queue Nodes
+typedef struct{
+    unsigned long index;
+    Pq *next;
 }Pq;
+
+unsigned long extract_min(Pq *pq){
+    Pq first = *pq;
+    unsigned long index = first.index;
+
+    *pq = 
+    return index;
+}
+
+typedef char Queue;
+enum whichQueue {NONE, OPEN, CLOSED};
+
+typedef struct {
+double g, h;
+unsigned long parent;
+Queue whq;
+} AStarStatus;
+
+double heuristic_dist(unsigned long index, node *nodes, AStarStatus *astar_status, )
